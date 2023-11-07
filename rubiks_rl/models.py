086b674ca9eb04591d5d45b1a422e21ed6383b8b
Long Hoang Dang <39938207+LongDangHoang@users.noreply.github.com>
2023-11-07 13:38:06 +1100

    Fix bug with model treating batch dimension as sequence dimension

--- rubiks_rl/models.py
+++ rubiks_rl/models.py
@@ -8,15 +8,16 @@ class Block(nn.Module):
     def __init__(self):
         super(Block, self).__init__()
         self.act = nn.ELU()
-        self.fc1 = nn.Linear(54 * 6, 54 * 6)
-        self.attn = nn.MultiheadAttention(embed_dim=54*6, num_heads=1, batch_first=True)
-        self.bn = nn.BatchNorm1d(num_features=54*6)
+        self.fc1 = nn.Linear(6, 6)
+        self.attn = nn.MultiheadAttention(embed_dim=6, num_heads=1, batch_first=True)
+        self.ln = nn.LayerNorm(6)
     
     def forward(self, x):
-        z = self.bn(x)
         z = self.fc1(x)
         z = self.act(z)
-        z = self.bn(z)
+        z = self.ln(z)
+        
+        # cast N, E to N, 1, E and treat the sequence to have 
         z, _ = self.attn(z, z, z, need_weights=False)
         z = self.act(z)
         z = z + x
@@ -27,6 +28,7 @@ class RubikModel(nn.Module):
     def __init__(self, num_blocks: int):
         super(RubikModel, self).__init__()
         self.flatten = nn.Flatten()
+        self.embed = nn.Linear(6, 6)
         self.blocks = nn.Sequential(*[
             Block()
             for _ in range(num_blocks)
@@ -39,8 +41,9 @@ class RubikModel(nn.Module):
         )
 
     def forward(self, x) -> Tuple[torch.Tensor, torch.Tensor]:
-        x = self.flatten(x)
+        x = self.embed(x)
         x = self.blocks(x)
+        x = self.flatten(x)
         v = self.value_head(x)
         p = self.policy_head(x)
         return v, p
